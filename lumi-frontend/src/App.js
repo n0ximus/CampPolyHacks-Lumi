a8d1d292059985b6c9be4fd176f1177d10e1a2a7
noximus <SageM5862@gmail.com>
2023-01-21 15:59:18 -0800

    add home as root

--- lumi-frontend/src/App.js
+++ lumi-frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { useContext } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+              <Router>
+                  <Routes>
+                      <Route exact path="/" element={<Home />}>
+                      </Route>
+                  </Routes>
+              </Router>
       </header>
     </div>
   );
---
+++ lumi-frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Home.css"
+import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+
+    return (
+        <>
+            <div className="home">
+
+            </div>
+        </>
+    )
+}
