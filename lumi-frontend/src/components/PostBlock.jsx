840eef076d4bd7754e4b6aec21de728556e4989a
borajoo <chocobora1@gmail.com>
2023-01-21 19:44:40 -0800

    Basic PostBlocks

--- lumi-frontend/src/components/PostBlock.jsx
+++ lumi-frontend/src/components/PostBlock.jsx
@@ -5,19 +5,21 @@ import Tag from "./Tag";
 export default function PostBlock({ post }) {
     return (
         <div className="postBlock">
-            <div className="stardust">
-                <div class="arrowUp"></div>
-                {post.stardust}
-                <div class="arrowDown"></div>
-            </div>
-            <div className="postInfo">
-                <div className="tags">
-                {post.category.map((c) => (
-                    <Tag category={c} />
-                ))}
+            <div className={post.TW ? "blur" : "noBlur"}>
+                <div className="stardust">
+                    <div class="arrowUp"></div>
+                    {post.stardust}
+                    <div class="arrowDown"></div>
                 </div>
-                <div className="postSummaryDiv">
-                    <p className="postSummary">{post.body}</p>
+                <div className="postInfo">
+                    <div className="tags">
+                    {post.category.map((c) => (
+                        <Tag category={c} />
+                    ))}
+                    </div>
+                    <div className="postSummaryDiv">
+                        <p className="postSummary">{post.body}</p>
+                    </div>
                 </div>
             </div>
 
--- lumi-frontend/src/pages/Home.jsx
+++ lumi-frontend/src/pages/Home.jsx
@@ -4,8 +4,16 @@ import { useLocation } from "react-router";
 import { useEffect, useState } from "react";
 import PostMap from "../components/PostMap"
 let posts = [
-    { "body": "hello world", "stardust": 10, "category": ["advice", "lgbtq+"] },
-    { "body": "hello edde", "stardust": 5, "category": ["disability"] },
+    { "body": "hello world", 
+    "stardust": 10, 
+    "category": ["advice", "lgbtq+"],
+    "TW": true
+    },
+    { "body": "hello edde", 
+    "stardust": 5, 
+    "category": ["disability"],
+    "TW": false 
+    },
 
 ]
 export default function Home() {
