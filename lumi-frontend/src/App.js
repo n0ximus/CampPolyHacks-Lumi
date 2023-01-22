6240493a091ba02b37878bee70766c4036b1727d
Arjun Chopra <achopr05@calpoly.edu>
2023-01-21 22:18:52 -0800

    NavBar (WORKING!!) (probably)

--- lumi-frontend/src/App.js
+++ lumi-frontend/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx"
+import NavBar from "./components/navbar"
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
               <Router>
                   <Routes>
