9050bec51b2822b7f5eb7ca98aa987b7c7b753e7
borajoo <chocobora1@gmail.com>
2023-01-22 10:21:26 -0800

    Finish post page

--- lumi-frontend/src/pages/PostPage.jsx
+++ lumi-frontend/src/pages/PostPage.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect, useLocation} from 'react';
 import './PostPage.css'
 import { Link } from 'react-router-dom';
 import Tag from "../components/Tag";
 import CommentMap from "../components/CommentMap"
+import axios from "axios";
+
 
 export default function PostPage({post}) {
+    var comments = [{
+        post: "id",
+        body: "You can do it!",
+        starDust: 0,
+    },
+    {
+        post: "id",
+        body: "I feel you, I hope you figure it out!",
+        starDust: 1,
+    }]
 
     return (
-
+        <div className="postPageDiv">
             <div className="commentRantBlock">
-
                 <div className="stardust">
                     <div class="arrowUp"></div>
                     {post.stardust}
@@ -17,17 +28,21 @@ export default function PostPage({post}) {
                 </div>
                 <div className="postInfo">
                     <div className="tags">
-
                     {post.category.map((c) => (
                         <Tag category={c} />
                     ))}
-
-
                     </div>
-                    <div className="commentBox">
+                    <div className="postSummaryDiv">
                         <p className="postSummary">{post.body}</p>
                     </div>
+                    <p className="commentsHeader">Leave a comment:</p>
+                    <div className="commentInputDiv">
+                        <textarea type="text" placeholder="Comment..." className="writeCommentInput" autoFocus={true}/>
+                    </div>
+                    <p className="commentsHeader">Comments:</p>
+                    <CommentMap comment={comments}/>
+                </div>
             </div>
-            </div>
+        </div>
     )
 }
